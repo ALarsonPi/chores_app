ffdeff43d6541ab61175ca6a265e1c872320d0e6
Alec Larson <ALarsonPi@gmail.com>
2022-12-31 09:35:54 -0700

    changed all charts to empty on startup

--- lib/Providers/ChartProvider.dart
+++ lib/Providers/ChartProvider.dart
@@ -1,77 +1,12 @@
 import 'package:chore_app/Daos/ChartDao.dart';
-import 'package:chore_app/Global.dart';
 import 'package:flutter/cupertino.dart';
 
 import '../Models/frozen/Chart.dart';
 
 class ChartProvider extends ChangeNotifier {
-  List<String> circle1Text = [
-    "John",
-    "Jamie",
-    "Will",
-    "Abby",
-    // "Jake",
-    // "Johnny",
-    // "Santa",
-    // "Abe"
-  ];
-
-  List<String> circle2Text = [
-    "Clean/Clear Table",
-    "Pots/Pans",
-    "Dishwasher",
-    "Bathrooms",
-    // "Vaccuum (All carpet)",
-    // "Lawn and mow and lawn",
-    // "Clean Window and clean"
-  ];
-
-  List<String> circle3Text = [
-    "Wash Windows and Blinds",
-    "Dust Baseboards and Blinds",
-    "Mow lawn",
-    "Babysit baby Kylie",
-    // "Mop floors",
-    // "Give coal to yessir",
-    // "Beat the South yessir",
-    // "Run run run away yessir",
-  ];
-
-  late Chart exampleCircle = Chart(
-    id: "example1",
-    chartTitle: "Example Chart",
-    numberOfRings: 3,
-    ownerID: "",
-    tabNumForOwner: 0,
-    editorIDs: List.empty(),
-    viewerIDs: List.empty(),
-    circleOneText: circle1Text,
-    circleTwoText: circle2Text,
-    circleThreeText: circle3Text,
-  );
-
-  late Chart exampleCircle2 = Chart(
-    id: "example2",
-    chartTitle: "Second Chart",
-    numberOfRings: 2,
-    ownerID: "",
-    tabNumForOwner: 0,
-    editorIDs: List.empty(),
-    viewerIDs: List.empty(),
-    circleOneText: circle1Text,
-    circleTwoText: circle2Text,
-    circleThreeText: [],
-  );
-
-  // late var circleDataList = List<Chart>.filled(
-  //   Global.TABS_ALLOWED,
-  //   exampleCircle,
-  //   growable: false,
-  // );
-
   late var circleDataList = [
-    exampleCircle,
-    exampleCircle2,
+    Chart.emptyChart,
+    Chart.emptyChart,
     Chart.emptyChart,
   ];
 
